8eef301ea7b9c8fc1f89e37f1031ac94b14737aa
Mark Cornelisse <info@markcornelisse.nl>
2019-01-12 11:44:09 +0100

    Initial Commit

---
+++ whatismyip/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  whatismyip
+//
+//  Created by Mark Cornelisse on 12/01/2019.
+//  Copyright © 2019 Mark Cornelisse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
